cf203092093d221608393c6499a6660494a8dd82
Shivam Jain <shivamjain052@gmail.com>
2024-07-28 20:13:05 +0530

    update

--- swagger.js
+++ swagger.js
@@ -4,7 +4,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Book Management API",
+      title: "Book Management API With Role Based Authentication",
       version: "1.0.0",
     },
     components: {
